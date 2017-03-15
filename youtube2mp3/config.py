38c5f2ea5e5e5eb3d92c4042cd10b14af6d35f5d
Yuri Kroz <splanger@gmail.com>
2017-03-15 11:14:25 +0200

    Make configurations DRY, add comments describing HOST and PORT

--- youtube2mp3/config.py
+++ youtube2mp3/config.py
@@ -8,10 +8,15 @@ class AppConfig:
     """Application configuration.
     Encapsulates run-time resources and settings utilized throughout API resources.
     """
-    """Initialize application's configs"""
     _STATIC_DIR = 'static'
     STATIC_PATH = '/%s'.format(_STATIC_DIR)
+
+    # A port on which the application will be running.
+    # In production it is recommended to leave it as is and redirect to it
+    # from a production web server (e.g. nginx or apache).
     PORT = 5000
+
+    # HOST is used to build the URL from which a scraped MP3 file can be downloaded
     HOST = 'http://127.0.0.1:' + PORT.__str__()
 
     # Where scraped stuff should be saved
@@ -23,8 +28,7 @@ class AppConfig:
 
 class ProductionConfig(AppConfig):
     DEBUG = False
-    HOST = 'http://127.0.0.1'
-    PORT = 80
+    HOST = 'http://yourdomain.com'
 
     def __init__(self):
         super().__init__()
@@ -33,8 +37,6 @@ class ProductionConfig(AppConfig):
 class DevelopmentConfig(AppConfig):
     DEVELOPMENT = True
     DEBUG = True
-    PORT = 5000
-    HOST = 'http://127.0.0.1:' + PORT.__str__()
 
     def __init__(self):
         super().__init__()
